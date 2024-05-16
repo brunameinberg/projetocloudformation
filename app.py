6004907bd265cea997a98e4ea56e781eec84cbc4
brunameinberg <brunalm2@al.insper.edu.br>
2024-05-16 17:56:28 -0300

    atualiza

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ table = dynamodb.Table('MyDynamoDBTable')
 
 app = Flask(__name__)
 
-@app.route('/db', methods=['GET'])
+@app.route('/', methods=['GET'])
 def read_from_dynamodb():
     response = table.scan()
     items = response['Items']
@@ -18,6 +18,7 @@ def write_to_dynamodb():
     data = {'example_key': 'example_value'}
     table.put_item(Item=data)
     return 'Item adicionado à tabela DynamoDB com sucesso!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
