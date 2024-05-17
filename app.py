ee3ccd3965731e1c76458bf7171ec92323ff96c9
brunameinberg <brunalm2@al.insper.edu.br>
2024-05-17 11:24:21 -0300

    att

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 from flask import Flask, render_template, request
 
-import boto3
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('MyDynamoDBTable')
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def read_from_dynamodb():
-    response = table.scan()
-    items = response['Items']
-    return f'Dados da tabela DynamoDB: {items}'
-
-@app.route('/db', methods=['POST'])
-def write_to_dynamodb():
-    data = {'example_key': 'example_value'}
-    table.put_item(Item=data)
-    return 'Item adicionado à tabela DynamoDB com sucesso!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
