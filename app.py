6197c1cec207ccc4e3a513149d51d0a859f9451f
brunameinberg <brunalm2@al.insper.edu.br>
2024-05-17 14:05:31 -0300

    att app

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('./formulario.html')
+    return render_template('formulario.html')
 
 
 if __name__ == '__main__':
