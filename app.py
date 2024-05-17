7cb1db0a2933b9c4b383534b31d37f9cd198a237
brunameinberg <brunalm2@al.insper.edu.br>
2024-05-17 13:48:29 -0300

    ajuste

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('formulario.html')
+    return render_template('./formulario.html')
 
 
 if __name__ == '__main__':
