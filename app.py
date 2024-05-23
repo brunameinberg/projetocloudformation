557c9f84ed33d7f9e20102845981b85c30077daa
brunameinberg <brunalm2@al.insper.edu.br>
2024-05-23 18:33:55 -0300

    att

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import uuid
 import logging
 from botocore.exceptions import ClientError
 
-
 app = Flask(__name__, template_folder='templates')
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 table = dynamodb.Table('MyDynamoDBTable')
 
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -19,30 +19,31 @@ def teste():
     try:
         response = table.scan()
         mensagens = [item['mensagem'] for item in response.get('Items', [])]
+        logging.info(f"Mensagens recuperadas do DynamoDB: {mensagens}")
         return render_template('teste.html', mensagens=mensagens)
     except Exception as e:
         logging.error(f"Erro ao buscar dados no DynamoDB: {e}")
         return render_template('teste.html', mensagens=[])
 
-
 @app.route('/salvar_dados', methods=['POST'])
 def salvar_dados():
     if request.method == 'POST':
         mensagem = request.form['mensagem']
-        unique_id = str(uuid.uuid4())  # Gera um UUID único
+        unique_id = str(uuid.uuid4())
 
         logging.info(f'Mensagem recebida: {mensagem}')
         logging.info(f'UUID gerado: {unique_id}')
 
         try:
-            table.put_item(Item={
-                'id': unique_id,  # Use o UUID como a chave de partição
+            response = table.put_item(Item={
+                'id': unique_id,
                 'mensagem': mensagem
             })
-
-            logging.info('Dados inseridos no DynamoDB com sucesso.')
+            logging.info(f'Dados inseridos no DynamoDB com sucesso: {response}')
+        except ClientError as e:
+            logging.error(f'Erro ao inserir dados no DynamoDB: {e.response["Error"]["Message"]}')
         except Exception as e:
-            logging.error(f'Erro ao inserir dados no DynamoDB: {e}')
+            logging.error(f'Erro geral ao inserir dados no DynamoDB: {e}')
 
         return '', 204  # Retorna um status de sucesso sem conteúdo
 
